a6b91c1644b9d9b33ea6995a918b5b1db222030d
wepowers <60437256+wepowers@users.noreply.github.com>
2020-02-24 09:17:05 -0600

    Update 2 Inventory
    Added comments to blocks of code to better understand.

--- inventory.py
+++ inventory.py
@@ -1,10 +1,11 @@
 # Course: CS 30
 # Period: 1
 # Date created: 2020/02/13
-# Date last modified: 2020/02/20
+# Date last modified: 2020/02/24
 # Name: William Powers
 # Description: Text Based Game
 
+# Obstacles that you can run into that will help you to win
 positive_obstacle = ['Speed Boost', '5 XP Power Up', '10 XP Power Up',
                      '20 XP Power Up']
 print(positive_obstacle)
@@ -20,6 +21,7 @@ print(sorted(positive_obstacle))
 
 print("\n")
 
+# Obstacles that you can run into that will make it harder to win
 negative_obstacle = ['Flat Tire', 'Dead End', 'Gas Refill', 'Traffic Jam']
 print(negative_obstacle)
 message = f"Oh no! {negative_obstacle[0]}."
@@ -36,6 +38,7 @@ print(negative_obstacle)
 
 print("\n")
 
+# List of what you have in your inventory
 inventory = ['50 XP', 'Speed Boost', 'Free Life']
 message = f"Your inventory includes: {inventory}."
 print(message)
